77e6fb774dcd2b23f534670fa52a2fc01a75232a
kritidobra-25 <kritidobra@gmail.com>
2026-05-08 16:07:17 +0000

    instalova async handler

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,23 +1,19 @@
-	const getTasks = (req, res) => {
- 		res.status(200).json({ message: "Get All Task" });
- 	}
+const asyncHandler = require("express-async-handler");
 
-const setTask = (req, res) => {
-  if (!req.body || !req.body.test) {
-		res.status(400)
-        	throw new Error("Please enter a task");
+const getTasks = asyncHandler((req, res) => {
+  res.status(200).json({ message: "Get All Task" });
+});
 
-	}
+const setTask = asyncHandler((req, res) => {
+  res.status(200).json({ message: "Create Task" });
+});
 
-	res.status(200).json({ message: "Create Task" });
-}
+const updateTask = asyncHandler((req, res) => {
+  res.status(200).json({ message: `Task ${req.params.id} updated.` });
+});
 
-const updateTask = (req, res) => {
-	res.status(200).json({ message: `Task ${req.params.id} updated.` });
-}
-
-const deleteTask = (req, res) => {
-	res.status(200).json({ message: `Task ${req.params.id}  deleted.` });
-}
+const deleteTask = asyncHandler((req, res) => {
+  res.status(200).json({ message: `Task ${req.params.id} deleted.` });
+});
 
 module.exports = { getTasks, setTask, updateTask, deleteTask };
