df14f3e0ef5567d34ee3b98152d1c9e227a95332
kritidobra-25 <kritidobra@gmail.com>
2026-05-08 16:32:50 +0000

    ndryshova passw e mongos

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 const { errorHandler } = require("./middleware/errorMiddleware");
-const dotenv = require('dotenv').config();
+
 const port = process.env.PORT || 8000;
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch((err) => console.log("MongoDB error ❌", err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/tasks", require("./routes/taskRoutes"));
+app.use(errorHandler);
 
-app.use( errorHandler );
-
-
-
-app.listen(port, () => console.log(`Server listening on ${port}`)) 
+app.listen(port, () => console.log(`Server listening on ${port}`));
