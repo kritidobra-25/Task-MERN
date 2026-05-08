2a411531bc97b98c3751fcad06f2b985637d7c87
kritidobra-25 <kritidobra@gmail.com>
2026-05-08 16:45:43 +0000

    lidha db

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const mongoose = require("mongoose");
+const connectDB = require("./connect/database");
 const { errorHandler } = require("./middleware/errorMiddleware");
 
 const port = process.env.PORT || 8000;
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected ✅"))
-  .catch((err) => console.log("MongoDB error ❌", err));
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
